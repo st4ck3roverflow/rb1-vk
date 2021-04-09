555db3c29cb88fd88adbf73cb14e452c1f6c38f0
arturyudin <enartur39@gmail.com>
2021-04-09 04:41:28 +0300

    fix group_id type, switch to heroku instance, remove sensitive data

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ import ast
 
 ver_msg = 'Raid bot community - v0.1 alpha - arturyudin.site - [club199568112|pwned solutions]'
 
-isHerokuInstance = False
+isHerokuInstance = True
 
 if isHerokuInstance:
     community_token = os.environ.get('vk_api_token')
-    group_id = os.environ.get('vk_group_id')
+    group_id = int(os.environ.get('vk_group_id'))
     message1 = utils.adjust_message_text(os.environ.get('message1'))
     message2 = utils.adjust_message_text(os.environ.get('message2'))
     message3 = utils.adjust_message_text(os.environ.get('message3'))
@@ -16,7 +16,7 @@ if isHerokuInstance:
     controllers = ast.literal_eval(os.environ.get('admins'))
 
 else:
-    community_token = "85f72365ea4c87422f30f995e7078efc179a99ffb7d3be96e081c09f683bd62ba4bc7adc32aa7da9caeaa"  # group token
+    community_token = ""  # group token
     group_id = 199568112  # vk group id without club
     message1 = utils.adjust_message_text("pwned.solution test msg1")
     message2 = utils.adjust_message_text("pwned.solution test msg2")
